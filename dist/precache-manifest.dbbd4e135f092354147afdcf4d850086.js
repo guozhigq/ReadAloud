54c0c83bc9fe67397ba5977f2ef4cd3060cd4117
影子 <rr@yingzideMacBook-Pro.local>
2021-04-08 17:53:15 +0800

    feat:修改忽略文件

---
+++ dist/precache-manifest.dbbd4e135f092354147afdcf4d850086.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e965f439879f6b1373b10f1412e239b",
+    "url": "./public/manifest.json"
+  },
+  {
+    "revision": "f8cd10b74abe86bd5081",
+    "url": "css/app.601d2ada.css"
+  },
+  {
+    "revision": "71d90d7ab68e6c4e518857e76b49713b",
+    "url": "index.html"
+  },
+  {
+    "revision": "ca2131e7a160b5549e83",
+    "url": "js/about.d5be1ecf.js"
+  },
+  {
+    "revision": "f8cd10b74abe86bd5081",
+    "url": "js/app.df2a251e.js"
+  },
+  {
+    "revision": "9bf3899b32c7123a1eca",
+    "url": "js/chunk-vendors.2714703c.js"
+  },
+  {
+    "revision": "3c70c943820280932a2d859c9284673a",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
---
+++ dist/service-worker.js
@@ -0,0 +1,28 @@
+importScripts("precache-manifest.dbbd4e135f092354147afdcf4d850086.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// set the prefix and suffix of our sw's name
+workbox.core.setCacheNameDetails({
+    prefix: 'browse-exp',
+    suffix: 'v1.0.0',
+  });
+  // have our sw update and control a web page as soon as possible.
+  workbox.skipWaiting();
+  workbox.clientsClaim();
+  
+  // vue-cli3.0 supports pwa with the help of workbox-webpack-plugin, we need to get the precacheing list through this sentence.
+  workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+  
+  // cache our data, and use networkFirst strategy.
+  workbox.routing.registerRoute(
+    new RegExp('.*experiments\?.*'), 
+    workbox.strategies.networkFirst()
+  );
+  workbox.routing.registerRoute(
+    new RegExp('.*experiments/\\d'),
+    workbox.strategies.networkFirst()  
+  )
+  workbox.routing.registerRoute(
+    new RegExp('.*experiment_types.*'),
+    workbox.strategies.networkFirst()
+  )
+  
