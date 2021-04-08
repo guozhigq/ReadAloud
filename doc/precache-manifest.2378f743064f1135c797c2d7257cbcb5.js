1cbf44b6a67b8db88c5703f081b63da2f7eb2300
影子 <rr@yingzideMacBook-Pro.local>
2021-04-08 17:57:38 +0800

    feat:修改webpack打包目录

---
+++ doc/precache-manifest.2378f743064f1135c797c2d7257cbcb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401e2f1783a14c231150187cb36272b0",
+    "url": "./public/manifest.json"
+  },
+  {
+    "revision": "f8cd10b74abe86bd5081",
+    "url": "css/app.601d2ada.css"
+  },
+  {
+    "revision": "71d90d7ab68e6c4e518857e76b49713b",
+    "url": "index.html"
+  },
+  {
+    "revision": "ca2131e7a160b5549e83",
+    "url": "js/about.d5be1ecf.js"
+  },
+  {
+    "revision": "f8cd10b74abe86bd5081",
+    "url": "js/app.df2a251e.js"
+  },
+  {
+    "revision": "9bf3899b32c7123a1eca",
+    "url": "js/chunk-vendors.2714703c.js"
+  },
+  {
+    "revision": "f52141609bef20b76bcde2a9db82d6a1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: './',
-    outputDir: 'dist',
+    outputDir: 'doc',
     lintOnSave: true,
     runtimeCompiler: false,
     chainWebpack: config => {
